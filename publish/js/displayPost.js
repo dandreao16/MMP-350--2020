6aa9d6ceacaf964ca9dae6d53a7d7b300a5ae89c
dandreao16 <47123811+dandreao16@users.noreply.github.com>
2020-03-04 19:49:14 -0500

    update

--- publish/js/displayPost.js
+++ publish/js/displayPost.js
@@ -1,16 +1,23 @@
  const posts = js.getEl('posts');
 
-function createPost(postData, userInfo, postId) {
+function createPost(postData, userInfo, postId, postDate, postAuthor) {
+	const post = js.createEl('div', 'post');
 
-    const post  = js.createEl('div' , 'post');
-    
-    posts.insertBefore(post, posts.firstElementChild);
-    
-    const text = js.createEl('div', 'post-text', postData.text);
-    post.appendChild(text);
-   
-    //post info
-    
-    //author 
-    //
+	posts.insertBefore(post, posts.firstElementChild);
+
+const text = js.createEl('div', 'post-text', postData.text);
+post.appendChild(text);
+
+const postInfo = js.createEl('div','post-info');
+post.appendChild(postInfo);
+
+const author = js.createEl('div', 'post-author', userInfo.displayName);
+post.appendChild(author);
+
+
+const date = js.createEl('div', 'post-date', js.formatDate(postData.date));
+
+postInfo.appendChild(date);
+
+console.log(date);	
 }
